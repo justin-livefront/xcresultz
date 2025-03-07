13f75bd438dc06523c1067e37108cf72552c96dd
Justin Magnini <justin@livefront.com>
2025-03-07 13:06:21 -0600

    upload summary artifact

--- Sources/Keanu/Keanu.swift
+++ Sources/Keanu/Keanu.swift
@@ -1,4 +1,5 @@
 import ArgumentParser
+import Foundation
 
 @main
 struct Keanu: ParsableCommand {
@@ -6,6 +7,25 @@ struct Keanu: ParsableCommand {
     @Option(help: "Path at which the command will be executed.") var workingDirectory: String = ""
 
     func run() throws {
-        print("Hello World!")
+        /* generate summary */
+        let html = """
+        <html>
+        <body>
+        <h1>Keanu Approves!</h1>
+        <p>
+        <img src="https://c.tenor.com/zsM4dGGpRUkAAAAC/woah-keanu-reeves.gif"/>
+        </p>
+        </body>
+        </html>
+        """
+
+        if #available(macOS 13.0, *) {
+            let summaryData = html.data(using: .utf8)
+            let path = URL(fileURLWithPath: workingDirectory)
+            let file = path.appendingPathComponent("summary.html", isDirectory: false)
+            print("Generating summary at \(file)")
+            try summaryData?.write(to: file, options: .atomic)
+        } else {
+        }
     }
 }
--- Sources/KeanuRunner/App.swift
+++
@@ -1,9 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-@main
-struct App {
-    static func main() {
-        print("Hello Keanu!")
-    }
-}
